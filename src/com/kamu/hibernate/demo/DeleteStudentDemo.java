a0c7e72110e92a527bb07eab65db1ac73df5a22e
Dang Kham <danqkhaw72@gmail.com>
2020-07-30 20:57:49 +0700

    Deleting objects with Hibernate

---
+++ src/com/kamu/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,62 @@
+package com.kamu.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kamu.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+	
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// crate session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			int studentId = 1;
+			
+			// now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// retrieve student based on the id : primary key
+			System.out.println("\nGetting student with id: " + studentId);
+			
+			Student mystudent = session.get(Student.class, studentId);
+			
+			// delete the student
+			//System.out.println("Deleting student: " + mystudent);
+			//session.delete(mystudent);
+			
+			// delete student id=2
+			System.out.println("Deleting student id=2");
+			session.createQuery("delete from Student where id=2").executeUpdate();
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for(Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+}
